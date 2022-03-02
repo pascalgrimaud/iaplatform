e7af8cb265236b4ee473bb7a898273865171bdbd
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-03-02 14:43:23 +0100

    add security

--- src/test/java/com/mycompany/myapp/IntegrationTest.java
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -6,11 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 
 @DisplayNameGeneration(ReplaceCamelCase.class)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(classes = { JhipsterApp.class })
 @Target(ElementType.TYPE)
+@WithMockUser
 public @interface IntegrationTest {
   public String[] properties() default {};
 }
